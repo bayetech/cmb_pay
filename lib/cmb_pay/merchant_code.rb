43b789c2e46d42bdc5f8dfe8b0f41c170f0ff73f
guochunzhong <guochunzhong@bayekeji.com>
2016-06-23 20:13:40 +0800

    Finish MerchantCode.generate

--- lib/cmb_pay/merchant_code.rb
+++ lib/cmb_pay/merchant_code.rb
@@ -13,13 +13,33 @@ module CmbPay
     # * +merchant_url+ - 商户接受通知的URL
     # * +payer_id+ - 付款方用户标识。用来唯一标识商户的一个用户。长度限制为40字节以内。
     # * +payee_id+ - 收款方的用户标识。生成规则同上。不要求商户提供用户的注册名称，但需要保证一个用户对应一个UserID。
-    # * +client_ip+ - 商户取得的客户端IP，如果有多个IP用逗号','分隔。长度限制为64字节。
+    # * +client_ips+ - 商户取得的客户端IP，如果有多个IP用逗号','分隔。长度限制为64字节。
     # * +goods_type+ - 商品类型编码，长度限制为8字节。
     # * +reserved+ - 保留字段，长度限制为1024字节。
     def self.generate(random: '3.14', strkey:, date:, branch_id:, co_no:, bill_no:,
                       amount:, merchant_para:, merchant_url:,
-                      payer_id:, payee_id:, client_ip: nil, goods_type: nil, reserved: nil)
-      '|aGpDsEcbmOuYcSeT5rQhnl0Z18OKkuAJlvDJXaex3KJXCn7KJ9XYfiw*UhIW6/a*YRTH1ImLwYPMybevPLIOUx1y6WdEnfv84loW9JF8nvw3Hsv/IWQpLd80SawuxobNab5OMOxpLg==|acdc1b7113a47324c2209626d11fa632e1210b1c'
+                      payer_id:, payee_id:, client_ips: nil, goods_type: nil, reserved: nil)
+      last_3 = optional_last_3(client_ips: client_ips, goods_type: goods_type, reserved: reserved)
+      combine_part1 = pay_to_in_RC4(random: random, strkey: strkey, payer_id: payer_id, payee_id: payee_id, opt_last_3: last_3)
+      combine_part2 = "#{strkey}#{combine_part1}#{date}#{branch_id}#{co_no}#{bill_no}#{amount}#{merchant_para}#{merchant_url}"
+      "|#{combine_part1}|#{Sign::SHA1.digest(combine_part2)}"
+    end
+
+    private
+
+    def self.pay_to_in_RC4(random:, strkey:, payer_id:, payee_id:, opt_last_3:)
+      in_data = "#{random}|#{payer_id}<$CmbSplitter$>#{payee_id}#{opt_last_3}"
+      key_md5_digest = Sign::MD5key.digest(strkey)
+      encrypted = CmbPay::Sign::RC4.encrypt(key_md5_digest, in_data)
+      CmbPay::Sign::Base64.encode64(encrypted)
+    end
+
+    def self.optional_last_3(client_ips: nil, goods_type: nil, reserved: nil)
+      r = ''
+      r << "<$ClientIP$>#{client_ips}</$ClientIP$>" unless client_ips.nil?
+      r << "<$GoodsType$>#{goods_type}</$GoodsType$>" unless goods_type.nil?
+      r << "<$Reserved$>#{reserved}</$Reserved$>" unless reserved.nil?
+      r
     end
   end
 end
--- spec/cmb_pay/merchant_code_spec.rb
+++ spec/cmb_pay/merchant_code_spec.rb
@@ -9,7 +9,7 @@ describe CmbPay::MerchantCode do
     specify 'test case from CMB document' do
       expect(subject.generate(strkey: 'KeyString', date: '20081129', branch_id: '0755', co_no: '000354', bill_no: '0011223344',
                               amount: '12.43', merchant_para: 'MerchantParaValue', merchant_url: 'http://www.abc.com/bankReciev',
-                              payer_id: 'User1', payee_id: 'User2', client_ip: '202.97.113.23', goods_type: '00000000'))
+                              payer_id: 'User1', payee_id: 'User2', client_ips: '202.97.113.23', goods_type: '00000000'))
         .to eq '|aGpDsEcbmOuYcSeT5rQhnl0Z18OKkuAJlvDJXaex3KJXCn7KJ9XYfiw*UhIW6/a*YRTH1ImLwYPMybevPLIOUx1y6WdEnfv84loW9JF8nvw3Hsv/IWQpLd80SawuxobNab5OMOxpLg==|acdc1b7113a47324c2209626d11fa632e1210b1c'
     end
   end
