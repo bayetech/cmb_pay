40718b6a44b0f361eaa637ab3ad9db32a3fcef5b
guochunzhong <guochunzhong@bayekeji.com>
2016-06-24 22:53:09 +0800

    Add code coverage usage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
 SimpleCov.start
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'byebug'
 require 'cmb_pay'
