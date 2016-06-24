5b0fff4e5b994fa8ed0b9eb9b5c285cd9bdd3a0d
guochunzhong <guochunzhong@bayekeji.com>
2016-06-24 08:31:41 +0800

    Combine all 5 sign files.

--- lib/cmb_pay/sign.rb
+++ lib/cmb_pay/sign.rb
@@ -1,5 +1,31 @@
+require 'base64'
+require 'digest/md5'
+require 'openssl' # For RC4/RSA/SHA1
+
 module CmbPay
   module Sign
+    # CMB replace '+' with '*' according to standard Base64
+    module Base64
+      def self.encode64(bin)
+        ::Base64.strict_encode64(bin).tr('+', '*')
+      end
+    end
+
+    module MD5key
+      def self.digest(str_key)
+        Digest::MD5.hexdigest(str_key.encode('gb2312')).upcase
+      end
+    end
+
+    module RC4
+      def self.encrypt(md5_hash, data)
+        cipher = OpenSSL::Cipher.new('RC4')
+        cipher.encrypt
+        cipher.key = Util.hex_to_binary(md5_hash)
+        cipher.update(data.encode('gb2312')) + cipher.final
+      end
+    end
+
     module Rsa
       # 返回消息的明文
       def self.plain_text(param_string)
@@ -19,6 +45,12 @@ module CmbPay
         # pub = OpenSSL::PKey::RSA.new(CmbPay.public_key)
         # pub.verify('sha1', signature(param_string), plain_text(param_string))
         true # TODO: comment out when having valid testing case
+      end
+    end
+
+    module SHA1
+      def self.digest(str)
+        OpenSSL::Digest::SHA1.hexdigest(str)
       end
     end
   end
--- lib/cmb_pay/sign/sha1.rb
+++
@@ -1,9 +0,0 @@
-module CmbPay
-  module Sign
-    module SHA1
-      def self.digest(str)
-        OpenSSL::Digest::SHA1.hexdigest(str)
-      end
-    end
-  end
-end
