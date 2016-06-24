d3b612d81a3a62a50a60d26d8526957a93633c70
guochunzhong <guochunzhong@bayekeji.com>
2016-06-24 18:04:14 +0800

    Avoid double || if possible.

--- lib/cmb_pay.rb
+++ lib/cmb_pay.rb
@@ -14,6 +14,7 @@ module CmbPay
     attr_accessor :expire_in_minutes # 会话有效时间
     attr_accessor :environment
   end
+  @expire_in_minutes = 30
   @environment = :production
 
   def self.uri_of_pre_pay_euserp(bill_no:, amount:, merchant_url:, merchant_para:,
@@ -21,14 +22,14 @@ module CmbPay
                                  options: {})
     branch_id = options.delete(:branch_id)
     co_no = options.delete(:co_no)
-    expire_time_span = options.delete(:expire_time_span)
+    expire_in_minutes = options.delete(:expire_in_minutes)
     uri_params = {
       'BranchID' => branch_id || CmbPay.branch_id,
       'CoNo'     => co_no || CmbPay.co_no,
       'BillNo'   => bill_no,
       'Amount'   => amount,
       'Date'     => Time.now.strftime('%Y%m%d'),
-      'ExpireTimeSpan' => expire_time_span || CmbPay.expire_in_minutes || 30,
+      'ExpireTimeSpan' => expire_in_minutes || CmbPay.expire_in_minutes,
       'MerchantUrl' => merchant_url,
       'MerchantPara' => merchant_para,
       'MerchantCode' => 'xx', # TODO: Need implement
