8abeae9e118aab1cdbaaabbdf6c51d818b118d82
guochunzhong <guochunzhong@bayekeji.com>
2016-07-09 20:22:05 +0800

    Add list of SUPPORTED_BANK

--- lib/cmb_pay.rb
+++ lib/cmb_pay.rb
@@ -23,6 +23,35 @@ module CmbPay
   @expire_in_minutes = 30
   @environment = :production
 
+  SUPPORTED_BANK = {
+    '招商银行' => nil,
+    '兴业银行' => 309,
+    '中信银行' => 302,
+    '中国民生银行' => 305,
+    '光大银行' => 303,
+    '华夏银行' => 304,
+    '北京农村商业银行' => 1418,
+    '深圳发展银行' => 307,
+    '中国银行' => 104,
+    '北京银行' => 403,
+    '中国邮政储蓄银行' => 100,
+    '上海浦东发展银行' => 310,
+    '东亚银行' => 2502,
+    '广东发展银行' => 306,
+    '南京银行' => 424,
+    '上海交通银行' => 301,
+    '平安银行' => 410,
+    '中国工商银行' => 102,
+    '杭州银行' => 423,
+    '中国建设银行' => 105,
+    '宁波银行' => 408,
+    '中国农业银行' => 103,
+    '浙商银行' => 316,
+    '渤海银行' => 318,
+    '上海农村商业银行' => 402,
+    '上海银行' => 313
+  }.freeze
+
   def self.uri_of_pre_pay_euserp(payer_id:, bill_no:, amount_in_cents:, merchant_url:, merchant_para: nil,
                                  protocol:, merchant_ret_url:, merchant_ret_para: nil,
                                  options: {})
@@ -33,11 +62,11 @@ module CmbPay
   end
 
   def self.uri_of_pre_pay_c2(bill_no:, amount_in_cents:, merchant_url:, merchant_para: nil,
-                             merchant_ret_url:, merchant_ret_para: nil, card_bank_id: nil,
+                             merchant_ret_url:, merchant_ret_para: nil, card_bank: nil,
                              options: {})
     generate_pay_link_of('PrePayC2',
                          nil, bill_no, amount_in_cents, merchant_url, merchant_para,
-                         nil, merchant_ret_url, merchant_ret_para, card_bank_id,
+                         nil, merchant_ret_url, merchant_ret_para, card_bank,
                          options)
   end
 
@@ -48,7 +77,7 @@ module CmbPay
   private_class_method
 
   def self.generate_pay_link_of(pay_type, payer_id, bill_no, amount_in_cents, merchant_url, merchant_para,
-                                protocol, merchant_ret_url, merchant_ret_para, card_bank_id, options)
+                                protocol, merchant_ret_url, merchant_ret_para, card_bank, options)
     branch_id = options.delete(:branch_id) || CmbPay.branch_id
     co_no = options.delete(:co_no) || CmbPay.co_no
     co_key = options.delete(:co_key) || CmbPay.co_key
@@ -71,11 +100,8 @@ module CmbPay
         'URL' => merchant_url,
         'Para' => cmb_merchant_para
       }.to_xml(root: 'Protocol', skip_instruct: true, skip_types: true, indent: 0)
-    elsif !card_bank_id.nil?
-      cmb_reserved_xml = "<CardBank>#{card_bank_id}</CardBank>"
-      payee_id = nil
     else
-      cmb_reserved_xml = nil
+      cmb_reserved_xml = generate_cmb_card_bank_xml(card_bank)
       payee_id = nil
     end
     m_code = MerchantCode.generate(random: random, strkey: co_key, date: trade_date,
@@ -98,5 +124,10 @@ module CmbPay
       'MerchantRetPara' => merchant_ret_para
     }
     Service.request_uri(pay_type, uri_params)
+  end
+
+  def self.generate_cmb_card_bank_xml(card_bank)
+    return nil if SUPPORTED_BANK[card_bank].nil?
+    "<CardBank>#{card_bank}</CardBank>"
   end
 end
