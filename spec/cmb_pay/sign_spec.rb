35dfbb0c9a9cc450b7825d89308d6604e4a06beb
guochunzhong <guochunzhong@bayekeji.com>
2016-08-04 17:50:55 +0800

    SHA1 digest from abc, same.

--- spec/cmb_pay/sign_spec.rb
+++ spec/cmb_pay/sign_spec.rb
@@ -35,5 +35,10 @@ describe CmbPay::Sign do
       input = 'KeyStringaGpDsEcbmOuYcSeT5rQhnl0Z18OKkuAJlvDJXaex3KJXCn7KJ9XYfiw*UhIW6/a*YRTH1ImLwYPMybevPLIOUx1y6WdEnfv84loW9JF8nvw3Hsv/IWQpLd80SawuxobNab5OMOxpLg==200811290755000354001122334412.43MerchantParaValuehttp://www.abc.com/bankReciev'
       expect(subject.sha1_digest(input)).to eq 'acdc1b7113a47324c2209626d11fa632e1210b1c'
     end
+
+    specify 'Another test from CMB bank' do
+      input = 'abc'
+      expect(subject.sha1_digest(input)).to eq 'a9993e364706816aba3e25717850c26c9cd0d89d'
+    end
   end
 end
