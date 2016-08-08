eb99748814364222c3d6c30555f7cdd31b07ca11
guochunzhong <guochunzhong@bayekeji.com>
2016-08-08 13:34:10 +0800

    No logic change, just improve the coding layout.

--- lib/cmb_pay/message/bill_records_message.rb
+++ lib/cmb_pay/message/bill_records_message.rb
@@ -14,12 +14,9 @@ module CmbPay
 
     def tag_start(name, _attributes)
       case name
-      when 'Head'
-        @in_head = true
-      when 'Body'
-        @in_body = true
-      when 'BllRecord'
-        @current_bill_record = {}
+      when 'Head'      then @in_head = true
+      when 'Body'      then @in_body = true
+      when 'BllRecord' then @current_bill_record = {}
       else
         @current_element_name = name
       end
@@ -27,51 +24,33 @@ module CmbPay
 
     def tag_end(name)
       case name
-      when 'Head'
-        @in_head = false
-      when 'Body'
-        @in_body = false
-      when 'BllRecord'
-        @bill_records << @current_bill_record
+      when 'Head'      then @in_head = false
+      when 'Body'      then @in_body = false
+      when 'BllRecord' then @bill_records << @current_bill_record
       end
     end
 
     def text(text)
       if @in_head
         case @current_element_name
-        when 'Code'
-          @code = text
-        when 'ErrMsg'
-          @error_message = text
+        when 'Code'   then @code = text
+        when 'ErrMsg' then @error_message = text
         end
       elsif @in_body
         case @current_element_name
-        when 'QryLopFlg'
-          @query_loop_flag = text
-        when 'QryLopBlk'
-          @query_loop_pos = text
-        when 'BillNo'
-          @current_bill_record[:bill_no] = text
-        when 'MchDate'
-          @current_bill_record[:merchant_date] = text
-        when 'StlDate'
-          @current_bill_record[:settled_date] = text
-        when 'BillState'
-          @current_bill_record[:bill_state] = text
-        when 'BillAmount'
-          @current_bill_record[:bill_amount] = text
-        when 'FeeAmount'
-          @current_bill_record[:fee_amount] = text
-        when 'CardType'
-          @current_bill_record[:card_type] = text
-        when 'BillRfn'
-          @current_bill_record[:bill_ref_no] = text
-        when 'BillType'
-          @current_bill_record[:bill_type] = text
-        when 'StlAmount'
-          @current_bill_record[:settled_amount] = text
-        when 'DecPayAmount'
-          @current_bill_record[:discount_pay_amount] = text
+        when 'QryLopFlg'    then @query_loop_flag = text
+        when 'QryLopBlk'    then @query_loop_pos = text
+        when 'BillNo'       then @current_bill_record[:bill_no] = text
+        when 'MchDate'      then @current_bill_record[:merchant_date] = text
+        when 'StlDate'      then @current_bill_record[:settled_date] = text
+        when 'BillState'    then @current_bill_record[:bill_state] = text
+        when 'BillAmount'   then @current_bill_record[:bill_amount] = text
+        when 'FeeAmount'    then @current_bill_record[:fee_amount] = text
+        when 'CardType'     then @current_bill_record[:card_type] = text
+        when 'BillRfn'      then @current_bill_record[:bill_ref_no] = text
+        when 'BillType'     then @current_bill_record[:bill_type] = text
+        when 'StlAmount'    then @current_bill_record[:settled_amount] = text
+        when 'DecPayAmount' then @current_bill_record[:discount_pay_amount] = text
         end
       end
     end
